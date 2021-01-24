e1d5da156d931f5a183ad7b280d278840e82fc40
William <williamduarte499@gmail.com>
2021-01-24 18:56:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ApolloProvider } from 'react-apollo';
+import Missions from './pages/Missions';
+import ApolloClient from './services/apollo';
+import { Container } from 'reactstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ApolloProvider client={ApolloClient}>
+        <Container className=' text-center'>
+          <h1 className='mt-5'>SpaceX Missions</h1>
+          <Missions />
+        </Container>
+      </ApolloProvider>
+    </>
   );
 }
 
---
+++ src/pages/Missions.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { useQuery } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Col, Collapse, Label, Row } from 'reactstrap';
+import styled from 'styled-components';
+import { FaChevronDown, FaChevronRight } from 'react-icons/fa';
+
+export default function Missions() {
+  const { loading, error, data } = useQuery(QuestsQuery);
+  if (error) {
+    return <h1 className='text-danger'>Fetch Error</h1>;
+  } else if (loading) {
+    return <h1 className='text-primary'>Loading...</h1>;
+  } else {
+    return (
+      <>
+        {data.missions.map((mission) => (
+          <MissionCollpase mission={mission} />
+        ))}
+      </>
+    );
+  }
+}
+
+const MissionCollpase = ({ mission }) => {
+  const [isOpen, setIsOpen] = useState();
+  return (
+    <MissionContainer onClick={() => setIsOpen(!isOpen)}>
+      <div className='d-flex'>
+        <strong className='mx-auto'>{mission.name}</strong>
+        {isOpen ? <FaChevronDown /> : <FaChevronRight />}
+      </div>
+      <Collapse isOpen={isOpen}>
+        <CollapseContent className='shadow-3'>
+          <Row>
+            <Col>
+              <Label className='m-0'>Website: </Label>
+              <a target='__blank' href={mission.website}>
+                <strong className='d-block'>{mission.website}</strong>
+              </a>
+              <Label className='m-0'>Manufacturers: </Label>
+              {mission.manufacturers.map((manufacturer) => (
+                <strong className='d-block'>{manufacturer}</strong>
+              ))}
+            </Col>
+            <Col>
+              <Label className='m-0'>Description: </Label>
+              <strong className='d-block'>{mission.description}</strong>
+            </Col>
+          </Row>
+        </CollapseContent>
+      </Collapse>
+    </MissionContainer>
+  );
+};
+
+const QuestsQuery = gql`
+  query {
+    missions {
+      name
+      manufacturers
+      twitter
+      payloads {
+        nationality
+        orbit
+      }
+      website
+      description
+    }
+  }
+`;
+
+const MissionContainer = styled.div`
+  align-items: center;
+  border-radius: 5px;
+  margin-top: 15px;
+  padding: 15px;
+  background-color: #eeeeee;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const CollapseContent = styled.div`
+  border-top: 1px solid black;
+  padding: 15px;
+  margin-top: 15px;
+`;
